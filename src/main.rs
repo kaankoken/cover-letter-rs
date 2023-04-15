204fa7194f0d61e31de0c5f79a6e513a07dabb1e
Kaan Taha Köken <kaan_taha_koken@hotmail.com>
2023-04-15 20:03:31 +0300

    feat(fileops): add file operation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,42 @@
-fn main() -> color_eyre::Result<()> {
+use color_eyre::{eyre::Context, Result};
+use std::{fs::File, io::BufReader, path::Path};
+
+fn check_file_ext(file_path: String) -> Result<()> {
+    if !file_path.ends_with(".json") && !file_path.ends_with(".arb") {
+        return Err(color_eyre::eyre::eyre!(
+            "file name must end with .json or .arb"
+        ));
+    }
+    Ok(())
+}
+
+fn check_file_exist<P: AsRef<Path>>(file_path: P) -> Result<()> {
+    let is_exist = file_path
+        .as_ref()
+        .try_exists()
+        .wrap_err("file permission related issue")?;
+
+    let is_file = file_path.as_ref().is_file();
+
+    if !is_exist {
+        return Err(color_eyre::eyre::eyre!("file does not exist"));
+    }
+
+    if !is_file {
+        return Err(color_eyre::eyre::eyre!("given path is not a file"));
+    }
+
+    Ok(())
+}
+
+fn read_files<P: AsRef<Path>>(file_path: P) -> Result<()> {
+    let file = File::open(file_path).wrap_err("file permission related issue")?;
+    let reader = BufReader::new(file);
+
+    Ok(())
+}
+
+fn main() -> Result<()> {
     println!("Hello, world!");
 
     Ok(())
